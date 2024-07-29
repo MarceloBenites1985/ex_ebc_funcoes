547828434cebc751926aaa6af7c9bbff8341179c
Marcelo Benites <157993443+MarceloBenites1985@users.noreply.github.com>
2024-07-28 23:39:22 -0300

    first commit

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+const resultado = multiplicacao(3, 4);
+const saudacaoMsg = saudacao("Marcelo");
+console.log(resultado);
+console.log(saudacaoMsg);
